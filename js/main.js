6ffca4afc5d26a6f7929ae19c84b0d48ca8c246f
Ajay Ullal <ajayullal91@gmail.com>
2016-10-19 22:37:29 +0530

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 
-$('#profilePic').on('change', function(){
-	
-	var $this = this;
-	var file = $this.file[0];
-	
-});
+var browserSupport = false;
+var browserSupport = window.File && window.FileReader;
+var file;
 
-$('#submitButton').on('click', function(){
-	
-});
+if(browserSupport){
+	$('#profilePic').on('change', function(){
+		var $this = this;
+		file = $this.files[0];
+	});
+
+	$('#submitButton').on('click', function(){
+		var $form = $form? $form: document.getElementById('registrationForm');
+		var formDate = new FormData($form);
+		if(file){
+			formDate.append('profilePic', file);
+		}
+		var formElements = $form.elements;
+		formDate.append('name', formElements[0].value);
+		formDate.append('email', formElements[1].value);
+		
+		for(var data of formDate.values()){
+			console.log(data);
+		}
+	});
+}else{
+	alert("Your browser doesn't support the HTML 5 File api. Please switch to an evergreen browser");
+}
